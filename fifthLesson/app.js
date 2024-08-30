2db14b0b6b39f21adfec05ea1ee40459e2b34754
DanyaEast <daniilshishko@mail.ru>
2024-08-30 16:12:45 +0200

    made pause

---
+++ fifthLesson/app.js
@@ -0,0 +1,63 @@
+// let titleProject = 'Оценка стоимости';
+// let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 20000;
+// let percentage = 50;
+let fullPrice = 60000;
+// let resposive = true;
+
+// titleProject = prompt ('Название проекта?')
+// screensValue = prompt ('Какие типы экранов?')
+// resposive = prompt ('Нужен ли респонсивный сайт?')
+// //Не нашёл как булевое значение в промпте попросить
+// if (resposive == 'Да') {
+//     resposive = true
+// } else {
+//     resposive = false
+// }
+
+// let service1 = prompt ('Какой сервис нужен?')
+// let servicePrice1 = +prompt ('Сколько это будет стоить?')
+// let service2 = prompt ('Какой еще сервис тебе нужен?')
+// let servicePrice2 = +prompt ('Сколько будет стоить этот второй сервис?')
+// fullPrice = screenPrice + servicePrice1 + servicePrice2
+
+// let servicePercentPrice = Math.round(fullPrice * 0.9)
+
+// console.log (servicePercentPrice)
+
+// if (servicePercentPrice >= 50000) {
+//     alert ('Сделаем скидку 10%')
+// } else if (servicePercentPrice >= 20000) {
+//     alert ('Cделаем скидку 5%')
+// } else if (servicePercentPrice >= 0) {
+//     alert ('Скидка не предусмотрена')
+// } else {
+//     alert ('Что-то пошло не так')
+// }
+
+
+
+// let servicePrice1 = 5
+// let servicePrice2 = 10
+// let allServicePrices = 0
+
+// let getAllServicePrices = function() {
+//     return servicePrice1 + servicePrice2
+// }
+
+// function getFullPrice() {
+//     return allServicePrices + screenPrice
+// }
+
+
+// allServicePrices = getAllServicePrices()
+// fullPrice = getFullPrice()
+let newTitle = 'ABCDE'
+
+function getTitle() {
+    return newTitle.toLowerCase()
+}
+
+let result = getTitle()
+console.log(result)
+
