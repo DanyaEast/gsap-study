54ee21aeb096fddccd05a2b4ea00c83346a4bdb0
DanyaEast <daniilshishko@mail.ru>
2024-08-30 10:32:32 +0200

    lessonNum3

---
+++ fourthLesson/app.js
@@ -0,0 +1,8 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 20000;
+let percentage = 50;
+let fullPrice = 60000;
+let resposive = true;
+
+console.log (titleProject)
